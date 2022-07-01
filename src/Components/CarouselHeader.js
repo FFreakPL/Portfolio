cae9e9f1096f4fbc99aa80e4cb6d49fed63df7db
Paweł Kubiak <ffreakpl@MacBook-Air-Pawe.local>
2022-07-01 17:06:36 +0200

    stylowanie carousel

---
+++ src/Components/CarouselHeader.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../css/out.css";
+
+function CarouselHeader() {
+
+    return (
+        <div className="carousel_header">
+            Make your choice
+        </div>
+    )
+}
+
+export default CarouselHeader;
--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -2,12 +2,20 @@ import React, { useState, useEffect} from 'react';
 import { useNavigate } from "react-router-dom";
 import "../css/out.css";
 import Header from "./Header";
+import Carousel, {CarouselItem} from "./Carousel";
+import CarouselHeader from "./CarouselHeader";
 
 function LandingPage() {
 
     return (
         <>
             <Header/>
+            <CarouselHeader/>
+            <Carousel>
+                <CarouselItem><div className="carousel_planyourride"/></CarouselItem>
+                <CarouselItem><div className="carousel_planyourride"/></CarouselItem>
+                <CarouselItem><div className="carousel_planyourride"/></CarouselItem>
+            </Carousel>
         </>
     )
 }
